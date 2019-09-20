d57cc04ed7ad91d0bd89d67fbb3687627bb22478
Julio Jose Capellan Vasquez <jcapellan@swisherdominicana.com>
2019-09-20 09:02:43 -0400

    incio del proyecto
    proyecto base previamente configurado para las apis

---
+++ src/main/groovy/com/swisherdominicana/molde/MoldeApplication.groovy
@@ -0,0 +1,13 @@
+package com.swisherdominicana.molde
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MoldeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MoldeApplication, args)
+    }
+
+}
